99099473e8c9714fb96d7bb7375938e23e16e11c
Jason Fulks <jason.fulks.2023@student.lhschools.org>
2020-02-18 12:21:32 -0500

    Variable inputs added

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,10 +1,11 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 drakeEquation();
-
     }
 
     public static void drakeEquation(){
@@ -18,6 +19,8 @@ fi = 1 (100% of which will develop intelligent life)
 fc = 0.1 to 0.2 (10–20% of which will be able to communicate)
 L = 1000 to 100,000,000 years (which will last somewhere between 1000 and 100,000,000 years)
  */
+Scanner  keyboard;
+keyboard = new Scanner(System.in);
 
         int N;
         int rStar;
@@ -27,12 +30,26 @@ L = 1000 to 100,000,000 years (which will last somewhere between 1000 and 100,00
         double f_i;
         double f_c;
         int L;
+        System.out.println("Give me the number of stars formed per year.");
+        rStar = keyboard.nextInt();
+        System.out.println("Give me the fraction in decimal form of stars that have planets.");
+        f_p = keyboard.nextDouble();
+        System.out.println("Give me the number of planets that will develop in the goldilocks zone.");
+        numberOfExoplanets = keyboard.nextInt();
+        System.out.println("Give me the percent in decimal form of these planets developing life.");
+        f_l = keyboard.nextDouble();
+        System.out.println("Give me the percent in decimal form of the number of life that will be intelligent.");
+        f_i = keyboard.nextDouble();
+        System.out.println("Give me the percent in decimal form of the intelligent life that will be able to communicate.");
+        f_c = keyboard.nextDouble();
+        System.out.println("Give me the number of years it will take to be able to communicate with them..");
+        L = keyboard.nextInt();
 
-        N = rStar * f_p * numberOfExoplanets * f_i * f_l * f_c * L
+        N = (int)(rStar * f_p * numberOfExoplanets * f_i * f_l * f_c * L);
 
 
         System.out.println("The number of intelligent species that are out there \n" +
-                "that can communicate with us is" +  N );
+                "that can communicate with us is " +  N );
     }
 
 }
